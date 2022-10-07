f67034fbda9e68d5d5ec442632d77acdc0b4795d
Cristhian R. Guerrero Soto <cristian.guerrero@guayas.gob.ec>
2022-10-07 11:20:48 -0500

    Scaffolding Lumen framework + JWT + GraphQL  del SGP+ | Modifico para que acepte todas las cabeceras del REQUEST.

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class CorsMiddleware
 {
@@ -11,25 +11,24 @@ class CorsMiddleware
      * Handle an incoming request.
      * Configuracion del CORS
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $req, Closure $next)
     {
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET,OPTIONS,PUT,DELETE',
+            // 'Access-Control-Allow-Headers'     => 'Content-Type,Authorization,X-Requested-With,x-token',
+            'Access-Control-Allow-Headers'     => '*',
+            'Access-Control-Allow-Methods'     => 'POST,GET,OPTIONS,PUT,DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             // 'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization,X-Requested-With',
         ];
 
-        if ($request->isMethod('OPTIONS')){
-            return response()->json(['method' => 'OPTIONS'], 200, $headers);
-        }
+        if ($req->isMethod('OPTIONS')) return response()->json(['method' => 'OPTIONS'], 200, $headers);
 
-        $response = $next($request);
+        $response = $next($req);
 
         foreach ($headers as $key => $value) {
             $response->header($key, $value);
