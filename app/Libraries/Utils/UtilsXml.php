80394273be8abbd5da8759d28c695ee0bcd39951
Cristhian R. Guerrero Soto <cristian.guerrero@guayas.gob.ec>
2022-10-11 10:41:30 -0500

    Scaffolding Lumen framework + JWT + GraphQL  del SGP+ | Optimización de la función xml2array()

--- app/Libraries/Utils/UtilsXml.php
+++ app/Libraries/Utils/UtilsXml.php
@@ -4,16 +4,24 @@ namespace App\Libraries\Utils;
 
 class UtilsXml
 {
+    /**
+     * Convetir un XML a un arreglo multidimensional
+     * @param string $xml
+     * @return array
+     */
     public static function xml2array($xml)
     {
         if ($xml == '') return [];
 
-        $simpleXmlObject = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?>'.$xml);
+        $simpleXmlObject = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?>' . $xml);
 
-        if($simpleXmlObject === false) return [];
+        if ($simpleXmlObject === false || empty($simpleXmlObject)) return [];
 
-        $arr = UtilsArray::array_change_key_case_recursive(json_decode(json_encode($simpleXmlObject),1));
+        $arr = current(json_decode(json_encode($simpleXmlObject), 1));
+        // Convertir los key names
+        if (UtilsString::isUpperCase(key($arr[0])))
+            $arr = UtilsArray::array_change_key_case_recursive($arr);
 
-        return current($arr);
+        return $arr;
     }
 }
